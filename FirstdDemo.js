19c2cf55595813d563ea6e52396083f6a5f965b5
Osama Bin Azam <osamabinazam@gmail.com>
2022-07-27 23:10:23 +0500

    first commit

---
+++ FirstdDemo.js
@@ -0,0 +1,26 @@
+document.writeln("Hello World, Welcome Mr. Osama");
+var number1 = 20;
+var number2=90;
+var number3 = number1+number2;
+console.log("The value of number3 is: "+number3);
+document.writeln("\nThe sum of "+number1 +"and " + number2 +" is: "+number3);
+
+
+{
+     var variable1 =40;
+    document.writeln(variable1*8);
+}
+variable1=90;
+document.writeln(variable1);
+
+
+var a=20;
+var b=50.5;
+var c='A'
+var d = "Hello Mr.Osama"
+var flag =true;
+document.writeln("The type of varibale a is: " + typeof(a));
+document.writeln("The type of varibale b is: "+ typeof(b));
+document.writeln("The type of varibale c is: "+ typeof(c));
+document.writeln("The type of varibale d is: "+ typeof(d));
+document.writeln("The type of varibale flag is: "+ typeof(flag));
