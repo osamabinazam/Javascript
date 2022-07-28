bcad1e2b334f6851c6ff0e3be7ac00ff6915632f
Osama Bin Azam <osamabinazam@gmail.com>
2022-07-28 13:43:20 +0500

    Learnt Operators and implemented ...

---
+++ Operators.js
@@ -0,0 +1,27 @@
+
+document.writeln("Arithemetic Operators: - - - - - - - - - - - - - - - - - - - - - - -  >  >  >  > ");
+
+document.writeln("Addition (+) : "+ (56+22) + "  ");
+document.writeln("Subtraction (-) : " + (56-22)+"  ");
+document.writeln("Multiplication (*) : " + (56*22)+"  ");
+document.writeln("Division (/) : " + (56/8)+"  ");
+document.writeln("Exponential (x^y or **) : " + (2**3)+"  ");
+document.writeln("Modulus (%) : " + (56%22)+"  ");
+var inc =56;
+document.writeln("Increment (++)) : " + (++inc) +"  ");
+document.writeln("Decrement (--) : " + (--inc) +"  ");
+
+document.writeln(" < < < < <- - - - - - - - - - -Assignment Operators: - - - - - - - - - - - - - - - - - - - - - - -  >  >  >  > ");
+var variable =10;
+document.writeln("Value of variable is: " +variable +" ");
+document.writeln("+= " + (variable+=variable) );
+document.writeln("-= " + (variable-=variable) );
+variable=5;
+document.writeln("*= " + (variable*=variable) );
+document.writeln("/= " + (variable/=variable) );
+document.writeln("%= " + (variable%=variable) );
+document.writeln("**= " + (variable**=variable) );
+
+
+
+
