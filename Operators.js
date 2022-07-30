88240ba501c83e0fbe423b0a2e9528666331d2d5
Osama Bin Azam <osamabinazam@gmail.com>
2022-07-30 13:54:52 +0500

    Learnt about Operators in JavaScript and implemented them and moving on control Statements

--- Operators.js
+++ Operators.js
@@ -1,26 +1,61 @@
 
-document.writeln("Arithemetic Operators: - - - - - - - - - - - - - - - - - - - - - - -  >  >  >  > ");
+document.writeln("<h2>Arithemetic Operators: </h2>");
 
-document.writeln("Addition (+) : "+ (56+22) + "  ");
-document.writeln("Subtraction (-) : " + (56-22)+"  ");
-document.writeln("Multiplication (*) : " + (56*22)+"  ");
-document.writeln("Division (/) : " + (56/8)+"  ");
-document.writeln("Exponential (x^y or **) : " + (2**3)+"  ");
-document.writeln("Modulus (%) : " + (56%22)+"  ");
+document.writeln("<br>Addition (+) : "+ (56+22) + " \n<br> ");
+document.writeln("Subtraction (-) : " + (56-22)+"  <br>");
+document.writeln("Multiplication (*) : " + (56*22)+" <br> ");
+document.writeln("Division (/) : " + (56/8)+"<br>  ");
+document.writeln("Exponential (x^y or **) : " + (2**3)+" <br> ");
+document.writeln("Modulus (%) : " + (56%22)+" <br> ");
 var inc =56;
-document.writeln("Increment (++)) : " + (++inc) +"  ");
-document.writeln("Decrement (--) : " + (--inc) +"  ");
+document.writeln("Increment (++)) : " + (++inc) +" <br> ");
+document.writeln("Decrement (--) : " + (--inc) +" <br> ");
 
-document.writeln(" < < < < <- - - - - - - - - - -Assignment Operators: - - - - - - - - - - - - - - - - - - - - - - -  >  >  >  > ");
+document.writeln("<h2>Assignment Operators: </h2><br>");
 var variable =10;
-document.writeln("Value of variable is: " +variable +" ");
-document.writeln("+= " + (variable+=variable) );
-document.writeln("-= " + (variable-=variable) );
+document.writeln("Value of variable is: " +variable +"<br> ");
+document.writeln("+= " + (variable+=variable)+"<br>" );
+document.writeln("-= " + (variable-=variable)+"<br>" );
 variable=5;
-document.writeln("*= " + (variable*=variable) );
-document.writeln("/= " + (variable/=variable) );
-document.writeln("%= " + (variable%=variable) );
-document.writeln("**= " + (variable**=variable) );
+document.writeln("*= " + (variable*=variable)+"<br>" );
+// document.writeln("/= " + (vntariable/=variable)+"<br>" );
+document.writeln("%= " + (variable%=variable)+"<br>" );
+document.writeln("**= " + (variable**=variable)+"<br>" );
+document.writeln("<h2>String Operators: </h2>");
+var str1 ="Hello";
+var str2 ="There";
+
+document.writeln("Concating (+) str1 and str2 is: " + (str1+str2));
+str1+=str1;
+document.writeln("<br>Concating and Assigning str1 with str1 is :  "+ (str1));
+
+document.writeln("<h2>Conditional Operators: </h2>");
+document.writeln("<h4>Using Ternary Operator: I Practiced all connditional operators:</h4>");
+
+var x = 10;
+document.writeln("Value of x is: " + x +"<br>");
+
+var valueofx = (x>=10)? "true":"false";
+document.writeln("x>=20  :"+valueofx);
+document.writeln("<br> x==10 :" + valueofx);
+x=9;
+document.writeln("<br>Now Value of x is: " + x +"<br>");
+document.writeln("x<=10 : " + valueofx);
+valueofx = (x==="9") ? "true" : "false";
+document.writeln("<br> x==='9' :" + valueofx);
+
+document.writeln("<h2>Logical Operators: </h2><br>");
+document.writeln("AND (&&), OR (||), Not (!) <br>");
+
+
+let x1=10 ;
+let y=20;
+document.writeln("Value of x1: " + x1 +"<br>Value of y is : "+ y);
+if(x1 > 5&& y<=20)
+    document.writeln("x1>5 && y<=20 : "+"True<br>");
+
+document.writeln("<h2>Bitwise Operators: </h2><br>");
+document.writeln("AND (&), OR (|), NOT (~), X-OR (^) <br>");
 
 
 
