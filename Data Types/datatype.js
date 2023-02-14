37d5d5426e71639f463f2a1e4ec0e455979921ba
Osama Bin Azam <osamabinazam@gmail.com>
2023-02-14 19:00:04 +0500

    Data Types

---
+++ Data Types/datatype.js
@@ -0,0 +1,47 @@
+document.write("<br> Hello from datatype.js<br> ");
+document.write("<h3> Numbers : </h3>")
+
+let number2 = 34
+let number1 = 67.87
+
+var str1 = "Type of " + number1 +" and "+ number2+ " is : " + typeof number1 + " and " + typeof number2
+document.write(str1)
+
+document.write( "<h3> Strings :</h3>")
+
+let string1 = "Hello World"
+ str1 = "Type of " + string1 + " is : " + typeof string1
+ document.write(str1)
+
+ document.write( "<h3> Boolean :</h3>")
+
+let boolvar = true
+ str1 = "Type of " + boolvar + " is : " + typeof boolvar
+ document.write(str1)
+
+ document.write( "<h3> Undefined :</h3>")
+
+ let undef ;
+  str1 = "Type of undef variale  is : " + typeof undef
+  document.write(str1)
+
+
+ document.write( "<h3> Object :</h3>")
+
+ const person = {firstname : "Osama" , lastname : "Arain"}
+
+ str1 = "Type of " + person  + " is : " + typeof person
+ document.write(str1)
+
+ document.write( "<h3> Arrays :</h3>")
+
+ const cars = ["Nisan GTR" , " Dodge Challenger", "Tayota Supra MK-4"]
+
+ str1 = "Type of " + cars  + " is : Array " + typeof cars
+ document.write(str1)
+
+
+
+
+ 
+
