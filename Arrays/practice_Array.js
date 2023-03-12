026a2a1fef8c80495a09e7c424483bc1f9f8dba9
Osama Bin Azam <osamabinazam@gmail.com>
2023-03-12 15:32:44 +0500

    Arrays and Objects

---
+++ Arrays/practice_Array.js
@@ -0,0 +1,19 @@
+
+var clothes = []
+clothes.push("T-Shirt")
+clothes.push("Jeans")
+clothes.push("Denom")
+clothes.push("Jacket")
+clothes.push("Dress pant")
+
+clothes.pop()
+
+clothes.push("Shirt")
+console.log(clothes[3])
+
+
+// Creating Object using dot notation
+var favCar = {}
+favCar.color="Black"
+favCar.covertible = true
+console.log(favCar)
